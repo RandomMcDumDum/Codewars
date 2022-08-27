903ae580194d0c3e433eb72dc1f4247fd1b8194c
Christian Rodriguez <cgrod12@gmail.com>
2022-08-26 21:03:50 -0500

    <8 kyu> Convert a Boolean to a String

---
+++ convertBoolToString.js
@@ -0,0 +1,9 @@
+// <8 kyu> Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+const booleanToString = b => b ? 'true' : 'false'
+
+// Test
+console.log(booleanToString(true)) // Expected: 'true'
+console.log(booleanToString(false)) // Expected: 'false'
