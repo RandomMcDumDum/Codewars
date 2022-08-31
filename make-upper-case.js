c3fae02cb077ab34bdf271142f206e2822f64e9f
RandomMcDumDum <cgrod12@gmail.com>
2022-08-31 07:05:04 -0500

    <8 kyu> MakeUpperCase

---
+++ make-upper-case.js
@@ -0,0 +1,9 @@
+// <8 kyu> MakeUpperCase
+
+// Write a function which converts the input string to uppercase.
+
+// My Solution
+let makeUpperCase = str => str.toUpperCase()
+
+// Test
+console.log(makeUpperCase("hello")) // Expected Output: HELLO
