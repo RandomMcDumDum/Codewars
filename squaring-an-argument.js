0383da220c4274aec85da9ad5b55da7eb43e12a5
Christian Rodriguez <cgrod12@gmail.com>
2023-01-07 15:30:49 -0600

    <8 kyu> Function 2 - squaring an argument

---
+++ squaring-an-argument.js
@@ -0,0 +1,12 @@
+// <8 kyu> Function 2 - squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+
+let square = (x) => x*x
+
+// Tests:
+console.log(square(3)) // Expected Output: 9
+console.log(square(2)) // Expected Output: 4
+console.log(square(9)) // Expected Output: 81
