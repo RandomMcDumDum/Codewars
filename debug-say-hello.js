50f53ec771383810ba9f5c58b88b02f350260cd9
Christian Rodriguez <christian@pop-os.localdomain>
2023-05-04 13:54:22 -0500

    <8 kyu> Debuy Say Hello

---
+++ debug-say-hello.js
@@ -0,0 +1,14 @@
+// Debugging sayHello function
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+let sayHello = name => `Hello, ${name}`
+
+// TESTS
+console.log(sayHello('Mr. Spock'))       //'Hello, Mr. Spock
+console.log(sayHello('Captain Kirk'))    //'Hello, Captain Kirk
+console.log(sayHello('Liutenant Uhura')) //'Hello, Liutenant Uhura
+console.log(sayHello('Dr. McCoy'))       //'Hello, Dr. McCoy
