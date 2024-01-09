1a77f1fe12f7e5cf25dd649b5ffc8186c3236c61
Christian Rodriguez <cgrod12@gmail.com>
2024-01-09 14:49:13 -0600

    <8 kyu> Are or Perimeter

---
+++ area-or-perimeter.js
@@ -0,0 +1,21 @@
+// <8 kyu> Are or perimeter
+//
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
+
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+// My Solution
+
+const areaOrPerimeter = function (l, w){
+    return (l == w ? l*w : (2*l + 2*w))
+}
+
+// TESTS
+console.log(areaOrPerimeter(3,  3)) //Expected Output:  9
+console.log(areaOrPerimeter(6, 10)) //Expected Output: 32
